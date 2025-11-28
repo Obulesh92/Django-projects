4a188486780c85a2625f88320810c478678cd680
OBULESHU KALVA <kallvaobulesh@gmail.com>
2025-11-28 22:18:21 +0530

    Delete todolist directory

--- todolist/Todolistapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Todo_data
-# Register your models here.
-# Removed the trailing dot that caused "Expected attribute name after '.'" syntax error.
-admin.site.register(Todo_data)
-# admin.register
