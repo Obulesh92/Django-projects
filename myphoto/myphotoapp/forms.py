a5721a3203ab66de3f5ca64918c132d0ae3d9b1d
OBULESHU KALVA <kallvaobulesh@gmail.com>
2025-11-29 16:31:40 +0530

    Delete myphoto directory

--- myphoto/myphotoapp/forms.py
+++
@@ -1,8 +0,0 @@
-# your_app_name/forms.py
-from django import forms
-from .models import MyVideo
-
-class VideoForm(forms.ModelForm):
-    class Meta:
-        model = MyVideo
-        fields = ['title', 'video']
