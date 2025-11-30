638852dca261e6a4417caf6c7ea24383b81def67
OBULESHU KALVA <kallvaobulesh@gmail.com>
2025-11-30 22:18:19 +0530

    Delete todolist directory

--- todolist/Todolistapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Todo_data(models.Model):
-    task = models.CharField(max_length=255)
-    def __str__(self):
-        return  self.task
