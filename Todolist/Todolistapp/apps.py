c2ca96f62be951de48b6e6c41a21e0017b49399c
OBULESHU KALVA <kallvaobulesh@gmail.com>
2026-01-16 23:02:26 +0530

    Delete Todolist directory

--- Todolist/Todolistapp/apps.py
+++
@@ -1,6 +0,0 @@
-# Todolistapp/apps.py
-from django.apps import AppConfig
-
-class TodolistappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Todolistapp'      # ← this must match
